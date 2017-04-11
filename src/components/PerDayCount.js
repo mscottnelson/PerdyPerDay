74ecbafe95f5bfc8fa19b3406158e352fdb46ce3
Michael Scott-Nelson <mscottnelson@gmail.com>
2017-04-11 16:33:44 -0400

    add fa icons

--- src/components/PerDayCount.js
+++ src/components/PerDayCount.js
@@ -1,5 +1,7 @@
-import { Component } from 'react';
 import '../stylesheets/ui.scss';
+import Coffee from 'react-icons/lib/fa/coffee';
+import Bike from 'react-icons/lib/fa/bicycle';
+import Calendar from 'react-icons/lib/fa/calendar';
 
 const percentToDecimal = (decimal) => {
         return ((decimal * 100) + '%');
@@ -13,14 +15,17 @@ export const PerDayCount = ({total, coffee, exercise, goal}) => (
     <div className="per-day-count">
         <div className="total-days">
             <span>{total}</span>
+            <Calendar />
             <span>days</span>
         </div>
         <div className="coffee-days">
             <span>{coffee}</span>
+            <Coffee />
             <span>days</span>
         </div>
         <div className="exercise-days">
             <span>{exercise}</span>
+            <Bike />
             <span>days</span>
         </div>
         <div>
