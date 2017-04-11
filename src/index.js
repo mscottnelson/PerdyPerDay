bf780fbe8851d3beaf6ce59ccbca48a619accd86
Michael Scott-Nelson <mscottnelson@gmail.com>
2017-04-11 15:56:20 -0400

    add React to window

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { PerDayCount } from './components/PerDayCount';
 
+window.React = React;
+
 render( 
     <PerDayCount />,
     document.getElementById('react-container')
